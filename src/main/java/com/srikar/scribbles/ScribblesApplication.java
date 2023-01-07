2deda3544b282e2e577520527e6beca49cbccc7c
Srikar KSV <srikar1awesome@gmail.com>
2023-01-07 17:12:05 +0530

    📦 NEW: Initial commit, added all the dependencies

---
+++ src/main/java/com/srikar/scribbles/ScribblesApplication.java
@@ -0,0 +1,13 @@
+package com.srikar.scribbles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribblesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScribblesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srikar/scribbles/ScribblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srikar.scribbles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribblesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
