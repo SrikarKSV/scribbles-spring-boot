272531e424ae5a06948fcd4700a1e822d6725771
Srikar KSV <srikar1awesome@gmail.com>
2023-01-07 19:45:32 +0530

    📦 NEW: Added form to add new scribbles

--- src/main/java/com/srikar/scribbles/controller/HomeController.java
+++ src/main/java/com/srikar/scribbles/controller/HomeController.java
@@ -3,13 +3,25 @@ package com.srikar.scribbles.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.srikar.scribbles.entity.Scribble;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Earth from HomeController");
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/scribble")
+    public String getScribble(Scribble scribble) {
+        return "form";
+    }
+
+    @PostMapping("/scribble")
+    public String postScribble() {
         return "home";
     }
 
